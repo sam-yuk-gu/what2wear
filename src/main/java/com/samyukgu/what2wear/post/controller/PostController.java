b209f0137da5e6a0607b2a7997505b5c01a8bc59
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-07-18 14:14:07 +0900

    feat: ãê²post screen design

---
+++ src/main/java/com/samyukgu/what2wear/post/controller/PostController.java
@@ -0,0 +1,8 @@
+package com.samyukgu.what2wear.post.controller;
+
+import javafx.event.ActionEvent;
+
+public class PostController {
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,6 @@ module com.samyukgu.what2wear {
 
 
     opens com.samyukgu.what2wear to javafx.fxml;
+    opens com.samyukgu.what2wear.post.controller to javafx.fxml;
     exports com.samyukgu.what2wear;
 }
