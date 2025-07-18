506b7f9dbc9ab97397ba63b358ca5c897e2736c2
mint723 <mint723dev@gmail.com>
2025-07-18 11:01:45 +0900

    feat: DI 컨테이너 생성(#1)

--- src/main/java/com/samyukgu/what2wear/HelloApplication.java
+++ src/main/java/com/samyukgu/what2wear/HelloApplication.java
@@ -1,5 +1,7 @@
 package com.samyukgu.what2wear;
 
+import com.samyukgu.config.ApplicationConfig;
+import com.samyukgu.di.DIContainer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,12 +11,24 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
+    public void init() throws Exception{
+        // 애플리케이션 시작 -> 의존관계 설정
+        ApplicationConfig.configure();
+    }
+
+    @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        // 애플리케이션 종료 -> container clear
+        DIContainer.getInstance().clear();
     }
 
     public static void main(String[] args) {
