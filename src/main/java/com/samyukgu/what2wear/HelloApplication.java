44e125be9ee8bb8f393ebe87269ec534d463d720
최이서 <chtoqur@gmail.com>
2025-07-18 17:52:46 +0900

    feat:  웰컴 메세지 구현(#9)

--- src/main/java/com/samyukgu/what2wear/HelloApplication.java
+++ src/main/java/com/samyukgu/what2wear/HelloApplication.java
@@ -5,10 +5,12 @@ import com.samyukgu.what2wear.di.DIContainer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,8 +21,8 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/samyukgu/what2wear/codi/CodiMainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1030, 768);
 
         // 전역 CSS 연결 및 폰트 등록
         URL styleUrl = getClass().getResource("/styles/style.css");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.samyukgu.what2wear {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    opens com.samyukgu.what2wear.codi.controller to javafx.fxml;
     opens com.samyukgu.what2wear to javafx.fxml;
     exports com.samyukgu.what2wear;
 }
