3072ccdbb5c005f0d548028aed2f257bf19de155
mint723 <mint723dev@gmail.com>
2025-07-18 16:49:44 +0900

    feat: JDBC 추가 (#7)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.samyukgu.what2wear {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.samyukgu.what2wear to javafx.fxml;
     exports com.samyukgu.what2wear;
