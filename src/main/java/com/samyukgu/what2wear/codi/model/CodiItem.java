40d595de27f27a9545b7c77f16d4e55230305121
Iseo Choi <chtoqur@gmail.com>
2025-07-21 00:28:45 +0900

    refactor: 롬복 적용

--- src/main/java/com/samyukgu/what2wear/codi/model/CodiItem.java
+++ src/main/java/com/samyukgu/what2wear/codi/model/CodiItem.java
@@ -1,40 +1,12 @@
 package com.samyukgu.what2wear.codi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class CodiItem {
     private String category;
     private String name;
     private String imagePath;
-
-    public CodiItem() {}
-
-    public CodiItem(String category, String name, String imagePath) {
-        this.category = category;
-        this.name = name;
-        this.imagePath = imagePath;
-    }
-
-    // getters, setters
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
 }
--- src/main/java/com/samyukgu/what2wear/codi/model/CodiSchedule.java
+++ src/main/java/com/samyukgu/what2wear/codi/model/CodiSchedule.java
@@ -1,59 +1,16 @@
 package com.samyukgu.what2wear.codi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class CodiSchedule {
-    private LocalDate date;
     private String description;
+    private LocalDate date;
     private List<CodiItem> codiItems;
     private ScheduleVisibility visibility;
-
-    // 기본 생성자
-    public CodiSchedule() {}
-
-    public CodiSchedule(String description, List<CodiItem> codiItems) {
-        this.description = description;
-        this.codiItems = codiItems;
-    }
-
-    public CodiSchedule(String description, LocalDate date, List<CodiItem> codiItems, ScheduleVisibility visibility) {
-        this.date = date;
-        this.description = description;
-        this.codiItems = codiItems;
-        this.visibility = visibility;
-    }
-
-    // getters, setters
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<CodiItem> getCodiItems() {
-        return codiItems;
-    }
-
-    public void setCodiItems(List<CodiItem> codiItems) {
-        this.codiItems = codiItems;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public ScheduleVisibility getVisibility() {
-        return visibility;
-    }
-
-    public void setVisibility(ScheduleVisibility visibility) {
-        this.visibility = visibility;
-    }
 }
