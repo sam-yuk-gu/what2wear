5d88378466514f3cd3328151ca681578a7e7e0cc
Iseo Choi <chtoqur@gmail.com>
2025-07-21 00:29:42 +0900

    chore: 롬복 설정 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,12 @@ module com.samyukgu.what2wear {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires lombok;
 
     opens com.samyukgu.what2wear to javafx.fxml;
     opens com.samyukgu.what2wear.post.controller to javafx.fxml;
+    opens com.samyukgu.what2wear.codi.controller to javafx.fxml;
+    opens com.samyukgu.what2wear.common.controller to javafx.fxml;
+
     exports com.samyukgu.what2wear;
 }
