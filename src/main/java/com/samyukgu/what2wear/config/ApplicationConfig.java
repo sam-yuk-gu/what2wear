7104ef1ec77c9ac2b03505061e82e525e2e11a28
mint723 <neu990112@gmail.com>
2025-07-21 00:08:13 +0900

    feat: MailService, AuthService 추가

--- src/main/java/com/samyukgu/what2wear/config/ApplicationConfig.java
+++ src/main/java/com/samyukgu/what2wear/config/ApplicationConfig.java
@@ -1,6 +1,8 @@
 package com.samyukgu.what2wear.config;
 
 import com.samyukgu.what2wear.di.DIContainer;
+import com.samyukgu.what2wear.mail.service.AuthService;
+import com.samyukgu.what2wear.mail.service.MailService;
 import com.samyukgu.what2wear.member.dao.MemberDAO;
 import com.samyukgu.what2wear.member.dao.MemberOracleDAO;
 import com.samyukgu.what2wear.member.service.MemberService;
@@ -21,5 +23,7 @@ public class ApplicationConfig {
         container.registerSingleton(MemberDAO.class, new MemberOracleDAO());
         // Service
         container.registerSingleton(MemberService.class, new MemberService(container.resolve(MemberDAO.class)));
+        container.registerSingleton(MailService.class, new MailService());
+        container.registerSingleton(AuthService.class, new AuthService(container.resolve(MailService.class)));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,13 @@ module com.samyukgu.what2wear {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jdk.jdi;
+    requires jakarta.mail;
 
     opens com.samyukgu.what2wear to javafx.fxml;
+    opens com.samyukgu.what2wear.common.controller to javafx.fxml;
+    opens com.samyukgu.what2wear.codi.controller to javafx.fxml;
+    opens com.samyukgu.what2wear.member.controller to javafx.fxml;
+
     exports com.samyukgu.what2wear;
 }
