4f26bc00da823ff9350adf04d76f8ec9977cf2b2
mint723 <mint723dev@gmail.com>
2025-07-22 21:12:07 +0900

    fix: MemberSession getter 추가(#24)

--- src/main/java/com/samyukgu/what2wear/member/Session/MemberSession.java
+++ src/main/java/com/samyukgu/what2wear/member/Session/MemberSession.java
@@ -3,11 +3,16 @@ package com.samyukgu.what2wear.member.Session;
 import com.samyukgu.what2wear.member.model.Member;
 
 public class MemberSession {
-    Member member;
+    private Member member;
 
     // 현재 세션의 회원 정보 저장
     public void setMember(Member member){
         this.member = member;
+    }
+
+    // 현재 세션의 회원 정보 불러오기
+    public Member getMember() {
+        return member;
     }
 
     // 회원 정보 초기화 (로그아웃 시 실행)
