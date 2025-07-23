2e6bd9b736df9022740d56efb745d3007dda26a1
최이서 <chtoqur@gmail.com>
2025-07-23 23:28:56 +0900

    fix: import 경로 변경

--- src/main/java/com/samyukgu/what2wear/myCodi/controller/DetailMyCodiController.java
+++ src/main/java/com/samyukgu/what2wear/myCodi/controller/DetailMyCodiController.java
@@ -1,6 +1,6 @@
 package com.samyukgu.what2wear.myCodi.controller;
 
-import com.samyukgu.what2wear.common.controller.MainLayoutController;
+import com.samyukgu.what2wear.layout.controller.MainLayoutController;
 import com.samyukgu.what2wear.di.DIContainer;
 import com.samyukgu.what2wear.member.Session.MemberSession;
 import com.samyukgu.what2wear.member.model.Member;
